ae1c4efa49d58b7faa594fd332b12a5f73d34ff5
Stéphane Becker <sbecker@methodinthemadness.eu>
2023-06-25 01:29:10 +0200

    feat( data-oriented ) : factorisation des données

---
+++ src/routes/+page.ts
@@ -0,0 +1,71 @@
+export function load() {
+    return {
+        entrees: [
+            {
+                plat: 'Oukha',
+                description: 'Velouté aux trois poissons (saumon, sandre et carpe).',
+                prix: 8.9
+            },
+            {
+                plat: 'Crêpe à la sibérienne',
+                description: 'Crêpe chaude à la viande hachée de bœuf grillé aux oignons et carottes au maïs, sauce fromage blanc, concombre et aneth.',
+                prix: 8.9
+            },
+            {
+                plat: 'Tartare de sandre de l\'Amou-Daria',
+                description: 'Tartare de sandre à la coriandre grillée, coriandre fraîche, oignons et paprika. Légèrement épicé.',
+                prix: 8.9
+            },
+            {
+                plat: 'Noix de St Jacques façon Mer Noire',
+                description: 'Noix de St Jacques marinées au citron et l\'huile d\'olive Kalamata, sauce poire et miel, caviar rouge de truite.',
+                prix: 9.9
+            },
+            {
+                plat: 'Pirojki (végétarien)',
+                description: 'Petit chausson au chou blanc sauté au paprika doux, caviar d\'aubergines.',
+                prix: 7.9
+            },
+            {
+                plat: 'Okroshka',
+                description: 'Soupe froide au lait fermenté, saucisson de porc, radis, concombres, œufs cuits, oignon vert, pommes de terre et l\'aneth.',
+                prix: 7.9
+            }
+        ],
+        plats: [
+            {
+                plat: 'Boeuf Stroganov',
+                description: 'Bœuf sauté, puis braisé avec une sauce à la crème et à l\'ail. Accompagné de la purée de pommes de terre.',
+                prix: 18.5,
+            },
+            {
+                plat: 'Canard façon tsar',
+                description: 'Magret de canard au miel et à la moutarde, petits pois verts, lardon, tomates cerises, cacahuètes.',
+                prix: 22.9,
+            },
+            {
+                plat: 'Gratin de berger de taïga\n',
+                description: 'Gratin d\'agneau à la purée de pommes de terre et patate douce, salade de la betterave rouge aux noix.',
+                prix: 18.5,
+            },
+            {
+                plat: 'Saumon à la Pojarski',
+                description: 'Saumon haché aux oignons grillés, carottes, pommes de terre, ciboulette et pané aux croûton de pain de mie, servie avec la poêlée de légumes.',
+                prix: 18.9,
+            },
+            {
+                plat: 'Grecciotto aux champignons (Végétarien)',
+                description: 'Risotto de graines de sarrasin grillées aux champignons de saison.',
+                prix: 15.5,
+            },
+            {
+                plat: 'Saucisse de Kazan',
+                description: 'Saucisse maison à la viande de poulet, poivrons, fromage et herbes fraîches. Servi avec écrasé de pommes de terre au fromage de brebis et coriandre fraiche.',
+                prix: 17.9,
+            },
+        ],
+        desserts: [
+
+        ]
+    };
+}
